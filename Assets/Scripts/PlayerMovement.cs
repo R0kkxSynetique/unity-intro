195bfdc049f689b291d1554182020b727ae67d58
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2023-02-15 18:20:12 +0100

    added player movements and camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float acceleration = 8f;
+    public float deceleration = 10f;
+    public float maxSpeed = 50f;
+    public float pitchSpeed = 50f;
+    public float rollSpeed = 50f;
+
+    [SerializeField]
+    private float _speed = 0f;
+    private float _pitchInput;
+    private float _accelerationInput;
+    private float _rollInput;
+    private float _yawInput;
+
     void Update()
     {
-        
+        // Get the inputs
+        _pitchInput = Input.GetAxis("Pitch");
+        _accelerationInput = Input.GetAxis("Acceleration");
+        _rollInput = Input.GetAxis("Roll");
+        _yawInput = Input.GetAxis("Yaw");
+
+        // Update the speed
+        _speed = _accelerationInput switch
+        {
+            // Acceleration
+            > 0 => Mathf.Min(_speed + acceleration * Time.deltaTime, maxSpeed),
+            // Deceleration
+            < 0 => Mathf.Max(_speed - deceleration * Time.deltaTime, 0),
+            // Keep speed
+            _ => _speed
+        };
+
+        // Apply movements
+
+        // Pitch (up/down)
+        transform.Rotate(Vector3.right * Time.deltaTime * pitchSpeed * _pitchInput);
+        // Forward
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        // Roll (left/right)
+        transform.Rotate(Vector3.forward * Time.deltaTime * rollSpeed * _rollInput);
+        // Yaw (turn left/right)
+        transform.Rotate(Vector3.up * Time.deltaTime * rollSpeed * _yawInput);
     }
 }
